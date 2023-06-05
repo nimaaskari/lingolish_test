64014b4904583734ed7821f96f2e8827fa0d020b
nimaaskari <nimaa.dev@gmail.com>
2023-06-05 19:38:58 +0330

    added the easy mode for the computer

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -22,8 +22,9 @@ function MainPage({ target }) {
     greenLetters: [],
     yellowLetters: [],
   });
+  const [winner, setWinner] = useState("");
 
-  let timeOutId;
+  // let timeOutId;
 
   function difficultyHandler(diff) {
     setDifficulty(diff);
@@ -62,52 +63,47 @@ function MainPage({ target }) {
         } else if (userGuess[i] === target[j]) {
           temp = "yellow";
         }
+
+        if (temp === "") temp = "gray";
+        compareResult.push(temp);
       }
-      if (temp === "") temp = "gray";
-      compareResult.push(temp);
+      console.log(compareResult);
+
+      setGuesses([
+        ...guesses,
+        { player: "user", word: userGuess, compareResult },
+      ]);
+
+      setUserInput([]);
+      setInputIndex(0);
+      setCountDown(0);
+      setPlayer("computer");
     }
-    console.log(compareResult);
-
-    setGuesses([
-      ...guesses,
-      { player: "user", word: userGuess, compareResult },
-    ]);
-
-    setUserInput([]);
-    setInputIndex(0);
-    setCountDown(0);
-    setPlayer("computer");
   }
 
   function mainGame() {
-    // if (countDown === 0) {
-    //   if (player === "user") {
-    //     setCountDown(10);
-    //     setPlayer("computer");
-    //   }
-    // }
     // countDown > 0 && setTimeout(() => setCountDown(countDown - 1), 1000);
     if (player === "computer") {
       computerGuess("easy", words);
     }
-    if (player === "user" && inputIndex === 4) {
-      userGuessChecker(target, userInput);
-    }
+    // if (player === "user" && inputIndex === 4) {
+    //   userGuessChecker(target, userInput);
+    // }
   }
 
   function computerGuess(difficulty = "easy", wordsArray) {
-    const randomIndex = Math.floor(Math.random() * words.length) + 1;
     if (
       computerUsedLetters.grayLetters.length === 0 &&
       computerUsedLetters.greenLetters.length === 0 &&
       computerUsedLetters.yellowLetters.length === 0
     ) {
+      const randomIndex = Math.floor(Math.random() * words.length) + 1;
       const computerFirstGuess = wordsArray[randomIndex];
       compare(computerFirstGuess, target);
       setGuesses([
         ...guesses,
         {
-          player: "user",
+          player: "computer",
           word: computerFirstGuess.split(""),
           compareResult: ["gray", "gray", "gray", "gray", "gray"],
         },
@@ -118,35 +114,87 @@ function MainPage({ target }) {
       setPlayer("user");
       return;
     }
-    // if (difficulty === "easy") {
-    // }
-    function compare(guess, targetWord) {
-      let tempComputerUsedLetters = { ...computerUsedLetters };
-      guess = guess.split("");
-      targetWord = target.split("");
-      guess.map((guessChar, guessIndex) => {
-        targetWord.map((targetWordChar, targetWordIndex) => {
-          if (guessChar === targetWordChar && guessIndex === targetWordIndex) {
-            tempComputerUsedLetters.greenLetters.push(guessChar);
-          } else if (guessChar === targetWordChar) {
-            tempComputerUsedLetters.yellowLetters.push(guessChar);
-          } else {
-            tempComputerUsedLetters.grayLetters.push(guessChar);
+    if (difficulty === "easy") {
+      // in this difficulty computer only checks if the word gray(irrelevant) characters or not.
+      let validWords = [];
+      for (let i = 0; i < wordsArray.length; i++) {
+        let valid = true;
+        for (let j = 0; j < wordsArray[i].split("").length; j++) {
+          if (
+            computerUsedLetters.grayLetters.includes(wordsArray[i].split("")[j])
+          ) {
+            valid = false;
           }
-        });
-      });
-      setComputerUsedLetters(tempComputerUsedLetters);
+        }
+        if (valid) {
+          validWords.push(wordsArray[i]);
+        }
+      }
+      console.log(target);
+      console.log(validWords);
+      console.log(computerUsedLetters);
+      const randomIndex = Math.floor(Math.random() * validWords.length) + 1;
+      const easyGuess = validWords[randomIndex];
+      compare(easyGuess, target);
+      setGuesses([
+        ...guesses,
+        {
+          player: "computer",
+          word: easyGuess.split(""),
+          compareResult: ["gray", "gray", "gray", "gray", "gray"],
+        },
+      ]);
+
+      setUserInput([]);
+      setInputIndex(0);
+      setPlayer("user");
+      return;
     }
   }
 
+  function compare(guessStr, targetWordStr) {
+    if (guessStr === targetWordStr) {
+      setWinner("computer");
+    }
+    let tempComputerUsedLetters = { ...computerUsedLetters };
+    const guess = guessStr.split("");
+    const targetWord = targetWordStr.split("");
+
+    for (let i = 0; i < guess.length; i++) {
+      for (let j = 0; j < targetWord.length; j++) {
+        if (
+          guess[i] === targetWord[j] &&
+          i === j &&
+          !tempComputerUsedLetters.greenLetters.includes(guess[i])
+        ) {
+          tempComputerUsedLetters.greenLetters.push(guess[i]);
+        } else if (
+          guess[i] === targetWord[j] &&
+          !tempComputerUsedLetters.yellowLetters.includes(guess[i])
+        ) {
+          tempComputerUsedLetters.yellowLetters.push(guess[i]);
+        } else if (!tempComputerUsedLetters.grayLetters.includes(guess[i])) {
+          tempComputerUsedLetters.grayLetters.push(guess[i]);
+        }
+      }
+    }
+    setComputerUsedLetters(tempComputerUsedLetters);
+  }
   useEffect(() => {
+    if (winner === "user") {
+      alert("You Win!");
+    } else if (winner === "computer") {
+      alert("You lost!");
+    }
+
     if (playing === true) {
       mainGame();
     }
-  }, [countDown, player, playing]);
+  }, [countDown, player, playing, winner]);
 
   return (
     <div className="MainPage">
+      {mainGame()}
       <StartMenu
         difficulty={difficulty}
         changeDifficulty={difficultyHandler}
